7926b15c6f4bff045f8cfd8fe3bc8556bf497048
Seu Nome <dancarlosazevedo@gmail.com>
2025-06-16 20:56:24 -0300

    deploy railway

--- sistematica/settings.py
+++ sistematica/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Segurança
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1").split(",")
 
 # Aplicativos instalados
 INSTALLED_APPS = [
