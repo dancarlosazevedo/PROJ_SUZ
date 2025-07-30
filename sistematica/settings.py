e85ef32c5666f54f9d675614aa1c2bb8ee8969f4
Seu Nome <dancarlosazevedo@gmail.com>
2025-07-30 20:39:39 -0300

    Container

--- sistematica/settings.py
+++ sistematica/settings.py
@@ -2,23 +2,22 @@ import os
 from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
+from django.urls import reverse_lazy
 
-# Caminho base do projeto
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# --- Configurações de Segurança e Ambiente ---
-# Pega a SECRET_KEY do arquivo .env
+
 SECRET_KEY = config('SECRET_KEY')
 
-# O modo DEBUG é controlado pelo .env (ex: DEBUG=True ou DEBUG=False)
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# ALLOWED_HOSTS é lido como uma lista de strings do .env
-# Exemplo no .env: ALLOWED_HOSTS=localhost,127.0.0.1,projsuz-production.up.railway.app
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
-# --- Aplicações Django ---
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -32,11 +31,9 @@ INSTALLED_APPS = [
     'simple_history',
 ]
 
-# --- Middleware ---
-# A ordem é importante
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # Whitenoise deve vir logo após o SecurityMiddleware
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,17 +44,15 @@ MIDDLEWARE = [
     'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
-# --- Configuração de URLs e Aplicação ---
+
 ROOT_URLCONF = 'sistematica.urls'
 WSGI_APPLICATION = 'sistematica.wsgi.application'
 
-# ... (depois da linha WSGI_APPLICATION)
 
-# --- Templates ---
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # Adiciona um diretório de templates na raiz do projeto, se você precisar
+
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -72,9 +67,7 @@ TEMPLATES = [
 ]
 
 
-# --- Banco de Dados ---
-# Configuração feita via DATABASE_URL do arquivo .env usando dj-database-url
-# Garante que a URL no .env use o nome do serviço do Docker (ex: @sistematica_db:5432)
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
@@ -82,14 +75,14 @@ DATABASES = {
 }
 
 
-# --- Validação de Senhas ---
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
 ]
 
 
-# --- Internacionalização ---
+
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
@@ -116,3 +109,5 @@ if os.getenv("RAILWAY_ENVIRONMENT"):
 
 # --- Chave Primária Padrão ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('core:home_calendario')
